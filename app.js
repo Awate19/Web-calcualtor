cc2a10cf27ffa884a416bca7763df9c01b87b53a
Awate19 <awega2019@gmail.com>
2019-08-06 00:49:19 -0700

    adding the functionality of the four Arthimetic operations

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+var ans
+var op=""
+var finalOp=""
 function abc()
 {
+    
 var z=["x0","x1","x2","x3","x4","x5","x6","x7","x8","x9",];
 for(i=0;i<z.length;i++)
 {
@@ -8,20 +12,73 @@ ele.value=i;
 ele.addEventListener("click", abcd);
   
 }
+var op=["plus","minus","times","divi"];
+for(i=0;i<op.length;i++)
+{
+var ele=document.getElementById(op[i]);
+ele.addEventListener("click", operators1);
+  
 }
+document.getElementById("eq").addEventListener("click",EQ)
+}
+
+function EQ()
+{
+   var num2=Number(document.getElementById("text1").value);
+if(finalOp=="plus")
+{
+ans=ans+num2;
+document.getElementById("text1").value=ans;
+}
+else if (finalOp=="minus")
+{
+ans=ans-num2;
+document.getElementById("text1").value=ans;
+}
+if(finalOp=="times")
+{
+ans=ans*num2;
+document.getElementById("text1").value=ans;
+}
+if(finalOp=="divi")
+{
+ans=ans/num2;
+document.getElementById("text1").value=ans;
+}
+}
+
+
+function operators1()
+{
+op=this.id;
+ans=Number(document.getElementById('text1').value);
+}
+
 
 function abcd()
 {
+    var xxx=this.value
     var txt=document.getElementById("text1");//this the input type text element
+    if(op=="")
+    {
     if (txt.value=="")
     {
-      
-        txt.value=this.value;
+        txt.value=xxx;
     }
     else
     {
         var num=txt.value;
-        num=num+this.value;
+        num=num+xxx;
         txt.value=num;
     }
+    }
+    else
+    {
+        txt.value="";
+        txt.value=xxx;
+        finalOp=op;
+    }
+
+
+
 }
